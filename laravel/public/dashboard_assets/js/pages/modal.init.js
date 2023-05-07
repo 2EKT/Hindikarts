b7f4d4cdc9c1b77a098a6d6565b3277c1d9dd2cf
2EK TECH <mashooda847@gmail.com>
2023-05-07 07:23:48 +0500

    First Bug fix update

--- laravel/public/dashboard_assets/js/pages/modal.init.js
+++
@@ -1,1 +0,0 @@
-var varyingcontentModal=document.getElementById("varyingcontentModal");varyingcontentModal&&varyingcontentModal.addEventListener("show.bs.modal",function(t){var e=t.relatedTarget.getAttribute("data-bs-whatever"),n=varyingcontentModal.querySelector(".modal-title"),t=varyingcontentModal.querySelector(".modal-body input");n.textContent="New message to "+e,t.value=e});
